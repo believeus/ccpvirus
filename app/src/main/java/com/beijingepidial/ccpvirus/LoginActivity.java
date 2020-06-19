4b36b3778aff75a65a7bc85e714c1ba19c704e26
Bonnie1001 <2388530535xr@sina.com>
2020-06-19 17:29:26 +0800

    Bonny

--- app/src/main/java/com/beijingepidial/ccpvirus/LoginActivity.java
+++ app/src/main/java/com/beijingepidial/ccpvirus/LoginActivity.java
@@ -1,22 +1,90 @@
 package com.beijingepidial.ccpvirus;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.Calendar;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText birthday;
+    private Spinner mSp_game_name;
 
+    @SuppressLint("WrongViewCast")
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
+        mSp_game_name = findViewById(R.id.spSars);
+        //Spinner默认null值
+        mSp_game_name.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                boolean b = true;
+                if (b) {
+                    // 主要功能代码；
+                    view.setVisibility(View.INVISIBLE);
+                } else {
+                    //获取到对象
+                    PhotoEntity photoEntity = (PhotoEntity) mSp_game_name.getSelectedItem();
+                }
+                b = false;
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+        //时间
+        birthday = (EditText) findViewById(R.id.etDate);
+        birthday.setInputType(InputType.TYPE_NULL); //不显示系统输入键盘</span>
+        birthday.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    showDatePickerDialog();
+                }
+            }
+        });
+        birthday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog();
+            }
+        });
         findViewById(R.id.btnNext).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(LoginActivity.this, PlateActivity.class));
             }
         });
+        findViewById(R.id.btnCancel).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            }
+        });
+    }
+    //时间
+    private void showDatePickerDialog() {
+        Calendar c = Calendar.getInstance();
+        new DatePickerDialog(LoginActivity.this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                birthday.setText(year + "/" + (monthOfYear + 1) + "/" + dayOfMonth);
+            }
+        }, c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH)).show();
+    }
+    private class PhotoEntity {
     }
 }
