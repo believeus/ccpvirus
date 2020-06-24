02b61bc6d5e1d222d2345d1be97cd2b5101e88d7
Bonnie1001 <2388530535xr@sina.com>
2020-06-24 15:40:36 +0800

    Bonny

--- app/src/main/java/com/beijingepidial/ccpvirus/LoginActivity.java
+++ app/src/main/java/com/beijingepidial/ccpvirus/LoginActivity.java
@@ -2,21 +2,26 @@ package com.beijingepidial.ccpvirus;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+
+import java.util.ArrayList;
 import java.util.Date;
+
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
 
 import java.text.SimpleDateFormat;
+import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
-//    private Spinner mSp_game_name;
 
     @SuppressLint("WrongViewCast")
     @RequiresApi(api = Build.VERSION_CODES.M)
@@ -24,28 +29,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
-
-//        mSp_game_name = findViewById(R.id.spSars);
-
-//        //Spinner默认null值
-//        mSp_game_name.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-//            @Override
-//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-//
-//                boolean b = true;
-//                if (b) {
-//                    // 主要功能代码；
-//                    view.setVisibility(View.INVISIBLE);
-//                } else {
-//                    //获取到对象
-//                    PhotoEntity photoEntity = (PhotoEntity) mSp_game_name.getSelectedItem();
-//                }
-//                b = false;
-//            }
-//            @Override
-//            public void onNothingSelected(AdapterView<?> parent) {
-//            }
-//        });
+//        Spinner spinner = findViewById(R.id.spSars);
+//        spinner.setSelection(1,true);
         //时间显示
         EditText time1 = findViewById(R.id.etDate);
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd  h:mm:ss a");
@@ -64,7 +49,9 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+
     private class PhotoEntity {
     }
+
 
 }
